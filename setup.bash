0f28494bddffe6a9ee8a5af571b3c978d0136d81
fahmyadan <fahmy.adan14@imperial.ac.uk>
2023-08-03 19:37:17 +0100

    fix python3 path

--- setup.bash
+++ setup.bash
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 
 source /opt/ros/galactic/setup.bash
-
 rosdep init 
-rosdep update --include-eol-distros 
+rosdep update --rosdistro=$ROS_DISTRO
 git submodule update --init
-python3 -m pip install setuptools gym pygame 
+/usr/bin/python3  -m pip install setuptools gym pygame 
+cd src/ros_gym/ros_gym/gym_carla/
+/usr/bin/python3 -m pip install -r requirements.txt
+/usr/bin/python3  -m pip install -e . 
 rosdep install --from-paths src --ignore-src -r -y --rosdistro galactic 
